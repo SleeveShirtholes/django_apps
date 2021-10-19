6f19fc4753f1f10d5e6ed501e4630b12139d3c71
Steve Schierholz <steven.schierholz@me.com>
2021-10-19 08:22:08 -0600

    completed django tutorial and started django/react tutorial

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import { Container } from 'react-bootstrap'
+
+import Header from './components/header'
+import Footer from './components/footer'
+import HomeScreen from './screens/HomeScreen'
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+        <HomeScreen/>
+        </Container>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
